0a0d6555cb268fe7fba12321058b973b3dcb0b3c
Fpril <fewpril@gmail.com>
2020-12-10 13:46:54 +0200

    cors

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const PORT = require('./Config/properties').PORT;
 const routes = require('./Routes/index.routes');
 
 app.use((request, response, next) => {
-    const allowedOrigins = ["http://localhost:4200"];
-    const origin = request.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        response.header("Access-Control-Allow-Origin", origin);
-    }
+    response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
     response.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
     next();
